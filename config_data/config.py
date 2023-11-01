0548d91c3a29dbb2bb8fe37324d2127feb4757f6
IgorLytkin <igor.lytkin.2020@ya.ru>
2023-11-01 20:08:31 +0700

    Добавил анализ aiogram 3.1.1, logging

--- config_data/config.py
+++ config_data/config.py
@@ -1,12 +1,12 @@
 from dataclasses import dataclass
-
 from environs import Env
 
 
 @dataclass
 class PaCredentials:
-    pa_login : str        # Логин в PT
+    pa_login: str        # Логин в PT
     pa_password: str      # Токен в PT
+
 
 @dataclass
 class Config:
@@ -14,11 +14,9 @@ class Config:
 
 
 # Создаем функцию, которая будет читать файл .env и возвращать
-# экземпляр класса Config с заполненными полями token и admin_ids
+# экземпляр класса Config с заполненными полями pa_login и pa_password
 def load_config(path: str | None = None) -> Config:
     env = Env()
     env.read_env(path)
-    return Config(
-         pa_login = env('pa_login'),
-         pa_password = env('pa_password')
-    )
+    return Config(PaCredentials(pa_login=env('PA_LOGIN'), pa_password=env('PA_PASSWORD')))
+
